0f380c050904625cde82224cf02fe3c5b5033a60
Luong Duc Tu <luongduc.tu1992@gmail.com>
2016-03-10 01:47:37 -0500

    Responsive footer

--- themes/minhnguyendesign/templates/footer.php
+++ themes/minhnguyendesign/templates/footer.php
@@ -1,11 +1,18 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <nav role="navigation" class="nav-box">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="menu-wrapper">
+      <div class="menu-wrapper__inner">
+        <nav role="navigation" class="nav-box">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
+          ?>
+          <div class="contact-box">
+            <div class="by"><span id="copyright">© Copyright 2014, Minh Nguyen Design Co .ltd Vietnam.</span></div>
+          </div>
+        </nav>
+      </div>
+    </div>
   </div>
 </footer>
